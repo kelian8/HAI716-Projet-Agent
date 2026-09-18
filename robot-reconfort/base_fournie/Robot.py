3e2bcd58da822d630747bcb5d7c68a00804b360d
Théo Hourmand <theohourmand.mail@gmail.com>
2026-09-18 17:07:56 +0200

    Creation classes Robot et Simulation

---
+++ robot-reconfort/base_fournie/Robot.py
@@ -0,0 +1,10 @@
+class Robot:
+
+    position
+    carte_connue
+    armoire_connue
+    objet_tenu = NULL
+
+    __init__(self,position,donnees):
+        self.position = position
+        self.carte_connue = NULL
