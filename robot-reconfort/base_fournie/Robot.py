840793a1cc05c1d9e038f919f1d42c72c02670c1
Théo Hourmand <theohourmand.mail@gmail.com>
2026-09-18 17:16:31 +0200

    push base propre

--- robot-reconfort/base_fournie/Robot.py
+++ robot-reconfort/base_fournie/Robot.py
@@ -5,6 +5,6 @@ class Robot:
     armoire_connue
     objet_tenu = NULL
 
-    __init__(self,position,donnees):
+    def __init__(self,position,donnees):
         self.position = position
         self.carte_connue = NULL
