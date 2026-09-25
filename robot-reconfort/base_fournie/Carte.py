5fb9b60079e9bac5c4feb35f8f0a2538df094839
Théo Hourmand <theohourmand.mail@gmail.com>
2026-09-25 17:44:23 +0200

    Désérialisation de Carte

--- robot-reconfort/base_fournie/Carte.py
+++ robot-reconfort/base_fournie/Carte.py
@@ -2,20 +2,29 @@ import reconfort_io as rio
 
 class Carte:
 
-    grille = 0
-    armoire_pos = 0
-    dico_pos = 0
-    #residents = dictionnaire{id -> (nom,pos)}
-    residents = 0 
+    def __init__(self, dimensions, grille, depart_robot, armoire_pos, dico_pos, residents):
+        self.dimensions = dimensions
+        self.grille = grille
+        self.depart_robot = depart_robot
+        self.armoire_pos = armoire_pos
+        self.dico_pos = dico_pos
+        self.residents = residents
 
-    #Objectif de l'intialisation : désirialiser le fichier donné au constructeur
-    def __init__(self,chemin):
+    @classmethod
+    def _from_json(cls,chemin):
         carte_donnees = rio.charger_carte(chemin)
-        self.grille = carte_donnees.get("grille")
-        self.armoire_pos = carte_donnees.get("armoire")
 
-    def __str__(self):
-        strng = "Données de la carte : \n"
-        #strng += "grille : " + self.grille + "\n"
-        strgn += "position de l'armoire" + self.armoire_pos + "\n"
-        return strgn
+        residents = {}
+        for r in carte_donnees["residents"]:
+            residents[r["id"]] = (r["nom"],tuple(r["position"]))
+
+        return cls(
+            dimensions = (carte_donnees["dimensions"]["hauteur"],carte_donnees["dimensions"]["largeur"]),
+            grille = carte_donnees["grille"],
+            depart_robot = tuple(carte_donnees["depart_robot"]),
+            armoire_pos = tuple(carte_donnees["armoire"]["position"]),
+            dico_pos = tuple(carte_donnees["dictionnaire"]["position"]),
+            residents = residents
+        )
+
+    
