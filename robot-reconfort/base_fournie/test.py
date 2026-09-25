e8c11cbb96be1c4801e924753257bc25ce60a976
Théo Hourmand <theohourmand.mail@gmail.com>
2026-09-25 17:44:52 +0200

    test désérialisation

--- robot-reconfort/base_fournie/test.py
+++ robot-reconfort/base_fournie/test.py
@@ -1,8 +1,15 @@
 import Scenario as sc
+import Carte
 
 def main():
     print("hello world")
-    scenario = sc.Scenario()
+    c = Carte.Carte._from_json("cartes/appartement_01.json")
+    print(c.dimensions)      # (9, 13)
+    print(c.grille[0])       # "#############"
+    print(c.depart_robot)    # (6, 1)
+    print(c.armoire_pos)     # (6, 4)
+    print(c.dico_pos)        # (7, 6)
+    print(c.residents)       # {'R1': ('Camille', (6, 11)), 'R2': ('Hugo', (3, 3))}
 
 if __name__ == "__main__":
     main()
